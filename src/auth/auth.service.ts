4d2bc16e370134b416ca3c209fbf62035bb1f5d3
Ruiz <alexander.rivail@gmail.com>
2023-06-26 10:22:22 -0300

    comment email signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -179,12 +179,12 @@ export class AuthService {
       hash,
     });
 
-    await this.mailService.userSignUp({
-      to: dto.email,
-      data: {
-        hash,
-      },
-    });
+    // await this.mailService.userSignUp({
+    //   to: dto.email,
+    //   data: {
+    //     hash,
+    //   },
+    // });
   }
 
   async confirmEmail(hash: string): Promise<void> {
