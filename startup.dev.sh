030828971a97c30025a2c04938ac5343c4fd6cfa
Ruiz <alexander.rivail@gmail.com>
2023-08-10 16:03:24 -0300

    test

--- startup.dev.sh
+++ startup.dev.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-# /opt/wait-for-it.sh localhost:5432
-# npm run migration:run
-# npm run seed:run
+/opt/wait-for-it.sh 10.192.2.21:5432
+npm run migration:run
+npm run seed:run
 npm run start:prod
