a23d974daa45622bd58f44e71b0368549cd6f311
Ruiz <alexander.rivail@gmail.com>
2023-10-05 13:37:39 -0300

    go back changes

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -2,10 +2,7 @@ import 'reflect-metadata';
 import { DataSource, DataSourceOptions } from 'typeorm';
 
 export const AppDataSource = new DataSource({
-  type:
-    process.env.DATABASE_TYPE !== undefined
-      ? process.env.DATABASE_TYPE
-      : 'postgres',
+  type: process.env.DATABASE_TYPE,
   url: process.env.DATABASE_URL,
   host: process.env.DATABASE_HOST,
   port: process.env.DATABASE_PORT
