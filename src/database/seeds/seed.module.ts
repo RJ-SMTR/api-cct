e70229b1b67ff32ffbcb5c31568226560a9896e4
Raphael Rivas <raphaelrivas@hotmail.com>
2023-11-24 11:09:02 -0300

    fix: seed only if db is empty

--- src/database/seeds/seed.module.ts
+++ src/database/seeds/seed.module.ts
@@ -3,22 +3,24 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import appConfig from 'src/config/app.config';
 import databaseConfig from 'src/config/database.config';
+import mailConfig from 'src/config/mail.config';
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { TypeOrmConfigService } from '../typeorm-config.service';
-import { RoleSeedModule } from './role/role-seed.module';
-import { StatusSeedModule } from './status/status-seed.module';
-import { UserSeedModule } from './user/user-seed.module';
-import { InfoSeedModule } from './info/info-seed.module';
 import { BankSeedModule } from './bank/bank-seed.module';
+import { InfoSeedModule } from './info/info-seed.module';
+import { InitSeedModule } from './init/init-seed.module';
+import { MailCountSeedModule } from './mail-count/mail-count-seed.module';
 import { InviteStatusSeedModule } from './mail-history-status/mail-history-status-seed.module';
+import { MailHistorySeedModule } from './mail-history/mail-history-seed.module';
+import { RoleSeedModule } from './role/role-seed.module';
 import { SettingTypeSeedModule } from './setting-type/setting-type.module';
 import { SettingSeedModule } from './setting/setting-seed.module';
-import { MailCountSeedModule } from './mail-count/mail-count-seed.module';
-import mailConfig from 'src/config/mail.config';
-import { MailHistorySeedModule } from './mail-history/mail-history-seed.module';
+import { StatusSeedModule } from './status/status-seed.module';
+import { UserSeedModule } from './user/user-seed.module';
 
 @Module({
   imports: [
+    InitSeedModule,
     RoleSeedModule,
     StatusSeedModule,
     UserSeedModule,
