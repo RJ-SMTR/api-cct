af3fff59c21a570fe1a057bfbc480b161adaba7c
williamfl2007 <williamfl2007@gmail.com>
2024-05-07 17:38:45 -0300

    Ajuste migrations 8

--- src/database/migrations/1714005101337-UpdateLancamento.ts
+++ src/database/migrations/1714005101337-UpdateLancamento.ts
@@ -5,13 +5,13 @@ export class UpdateLancamento1714005101337 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "lancamento" DROP COLUMN "anexo"`);
-        await queryRunner.query(`ALTER TABLE "lancamento" ADD "anexo" character varying`);
+        await queryRunner.query(`ALTER TABLE "lancamento" ADD "anexo" character`);
         await queryRunner.query(`ALTER TABLE "lancamento" DROP COLUMN "algoritmo"`);
-        await queryRunner.query(`ALTER TABLE "lancamento" ADD "algoritmo" character varying`);
+        await queryRunner.query(`ALTER TABLE "lancamento" ADD "algoritmo" character`);
         await queryRunner.query(`ALTER TABLE "lancamento" DROP COLUMN "glosa"`);
-        await queryRunner.query(`ALTER TABLE "lancamento" ADD "glosa" character varying`);
+        await queryRunner.query(`ALTER TABLE "lancamento" ADD "glosa" character`);
         await queryRunner.query(`ALTER TABLE "lancamento" DROP COLUMN "recurso"`);
-        await queryRunner.query(`ALTER TABLE "lancamento" ADD "recurso" character varying`);
+        await queryRunner.query(`ALTER TABLE "lancamento" ADD "recurso" character`);
         await queryRunner.query(`ALTER TABLE "lancamento" DROP COLUMN "valor_a_pagar"`);
         await queryRunner.query(`ALTER TABLE "lancamento" ADD "valor_a_pagar" numeric NOT NULL`);
         await queryRunner.query(`ALTER TABLE "lancamento" DROP COLUMN "numero_processo"`);
