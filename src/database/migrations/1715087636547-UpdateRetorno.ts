5b9275ef5dd61ade86f712a5c886c0ece978bda7
Raphael Rivas <raphaelrivas@hotmail.com>
2024-05-09 14:42:58 -0300

    fix: migrations

--- src/database/migrations/1715087636547-UpdateRetorno.ts
+++ src/database/migrations/1715087636547-UpdateRetorno.ts
@@ -15,7 +15,7 @@ export class UpdateRetorno1715087636547 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "detalhe_a" ADD CONSTRAINT "UQ_DetalheA_itemTransacao" UNIQUE ("itemTransacaoId")`);
         await queryRunner.query(`ALTER TABLE "ocorrencia" ADD CONSTRAINT "FK_TransacaoOcorrencia_headerLote_ManyToOne" FOREIGN KEY ("headerLoteId") REFERENCES "header_lote"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "detalhe_a" ADD CONSTRAINT "FK_DetalheA_itemTransacaoAgrupado_OneToOne" FOREIGN KEY ("itemTransacaoAgrupadoId") REFERENCES "item_transacao_agrupado"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE "dePtalhe_a" ADD CONSTRAINT "FK_DetalheA_itemTransacao_OneToOne" FOREIGN KEY ("itemTransacaoId") REFERENCES "item_transacao_agrupado"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "detalhe_a" ADD CONSTRAINT "FK_DetalheA_itemTransacao_OneToOne" FOREIGN KEY ("itemTransacaoId") REFERENCES "item_transacao_agrupado"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
