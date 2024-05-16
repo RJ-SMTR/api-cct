28f3e11ada27c5cf8a039d77948297c7e02b1049
Raphael Rivas <raphaelrivas@hotmail.com>
2024-05-15 23:36:51 -0300

    fix: dont run jobs on develop

--- src/cron-jobs/cron-jobs.service.ts
+++ src/cron-jobs/cron-jobs.service.ts
@@ -95,10 +95,6 @@ export class CronJobsService implements OnModuleInit, OnModuleLoad {
   async onModuleLoad() {
     const THIS_CLASS_WITH_METHOD = 'CronJobsService.onModuleLoad';
 
-    await this.saveTransacoesJae1();
-    await this.sendRemessa();
-    // await this.updateRetorno(); // hotfix/sftp-naming - 2024-05-10
-
     this.jobsConfig.push(
       {
         name: CrobJobsEnum.bulkSendInvites,
@@ -148,33 +144,33 @@ export class CronJobsService implements OnModuleInit, OnModuleLoad {
           },
         },
       },
-      {
-        name: CrobJobsEnum.saveTransacoesJae,
-        cronJobParameters: {
-          cronTime: '0 3 * * 5', // Every friday, 00:00 BRT = 03:00 GMT
-          onTick: async () => {
-            await this.saveTransacoesJae1();
-          },
-        },
-      },
-      {
-        name: CrobJobsEnum.sendRemessa,
-        cronJobParameters: {
-          cronTime: '0 4 * * 5', // Every friday, 01:00 BRT = 04:00 GMT
-          onTick: async () => {
-            await this.sendRemessa();
-          },
-        },
-      },
-      {
-        name: CrobJobsEnum.updateRetorno,
-        cronJobParameters: {
-          cronTime: '*/30 * * * *', // Every 30 min
-          onTick: async () => {
-            await this.updateRetorno();
-          },
-        },
-      },
+      // {
+      //   name: CrobJobsEnum.saveTransacoesJae,
+      //   cronJobParameters: {
+      //     cronTime: '0 3 * * 5', // Every friday, 00:00 BRT = 03:00 GMT
+      //     onTick: async () => {
+      //       await this.saveTransacoesJae1();
+      //     },
+      //   },
+      // },
+      // {
+      //   name: CrobJobsEnum.sendRemessa,
+      //   cronJobParameters: {
+      //     cronTime: '0 4 * * 5', // Every friday, 01:00 BRT = 04:00 GMT
+      //     onTick: async () => {
+      //       await this.sendRemessa();
+      //     },
+      //   },
+      // },
+      // {
+      //   name: CrobJobsEnum.updateRetorno,
+      //   cronJobParameters: {
+      //     cronTime: '*/30 * * * *', // Every 30 min
+      //     onTick: async () => {
+      //       await this.updateRetorno();
+      //     },
+      //   },
+      // },
       // {
       //   name: CrobJobsEnum.saveExtrato,
       //   cronJobParameters: {
