c299fb761967cc3139b0abcddbf5a9d4da42a3ca
Raphael Rivas <raphaelrivas@hotmail.com>
2024-05-24 06:28:43 -0300

    fix: run jobs exceptionally

--- src/cron-jobs/cron-jobs.service.ts
+++ src/cron-jobs/cron-jobs.service.ts
@@ -144,33 +144,35 @@ export class CronJobsService implements OnModuleInit, OnModuleLoad {
           },
         },
       },
-      // {
-      //   name: CrobJobsEnum.saveTransacoesJae,
-      //   cronJobParameters: {
-      //     cronTime: '0 3 * * 5', // Every friday, 00:00 BRT = 03:00 GMT
-      //     onTick: async () => {
-      //       await this.saveTransacoesJae1();
-      //     },
-      //   },
-      // },
-      // {
-      //   name: CrobJobsEnum.sendRemessa,
-      //   cronJobParameters: {
-      //     cronTime: '0 4 * * 5', // Every friday, 01:00 BRT = 04:00 GMT
-      //     onTick: async () => {
-      //       await this.sendRemessa();
-      //     },
-      //   },
-      // },
-      // {
-      //   name: CrobJobsEnum.updateRetorno,
-      //   cronJobParameters: {
-      //     cronTime: '*/30 * * * *', // Every 30 min
-      //     onTick: async () => {
-      //       await this.updateRetorno();
-      //     },
-      //   },
-      // },
+      {
+        name: CrobJobsEnum.saveTransacoesJae,
+        cronJobParameters: {
+          // cronTime: '0 3 * * 5', // Every friday, 00:00 BRT = 03:00 GMT
+          cronTime: '40 9 * * 5', // Every friday, 00:00 BRT = 03:00 GMT
+          onTick: async () => {
+            await this.saveTransacoesJae1();
+          },
+        },
+      },
+      {
+        name: CrobJobsEnum.sendRemessa,
+        cronJobParameters: {
+          // cronTime: '0 4 * * 5', // Every friday, 01:00 BRT = 04:00 GMT
+          cronTime: '40 10 * * 5', // Every friday, 01:00 BRT = 04:00 GMT
+          onTick: async () => {
+            await this.sendRemessa();
+          },
+        },
+      },
+      {
+        name: CrobJobsEnum.updateRetorno,
+        cronJobParameters: {
+          cronTime: '*/30 * * * *', // Every 30 min
+          onTick: async () => {
+            await this.updateRetorno();
+          },
+        },
+      },
       // {
       //   name: CrobJobsEnum.saveExtrato,
       //   cronJobParameters: {
