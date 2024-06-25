2a3e189e363aab7c3379c45745e22bb709eefcdd
Raphael Rivas <raphaelrivas@hotmail.com>
2024-06-25 16:36:45 -0300

    fix: message with no code

--- src/cnab/enums/ocorrencia.enum.ts
+++ src/cnab/enums/ocorrencia.enum.ts
@@ -94,5 +94,5 @@ export enum OcorrenciaEnum {
   'ZY' = 'Pagamento Rejeitado - Beneficiário Divergente',
   'ZW' = 'Dados do Pagador Incorretos',
   /** Ocorrências X são erros customizados do CCT. Não existem no CNAB */
-  '  ' = 'Ocorreu um erro! Por favor, aguarde a liberação do pagamento',
+  '  ' = 'Ocorreu um erro! Por favor, aguarde a liberação do pagamento.',
 }
