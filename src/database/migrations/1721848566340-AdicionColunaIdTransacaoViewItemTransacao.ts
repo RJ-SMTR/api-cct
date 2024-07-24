b4d2195ad9e66aa478eb7e957cdb930322261aa4
williamfl2007 <williamfl2007@gmail.com>
2024-07-24 19:26:30 -0300

    Ajuste migration

--- src/database/migrations/1721848566340-AdicionColunaIdTransacaoViewItemTransacao.ts
+++ src/database/migrations/1721848566340-AdicionColunaIdTransacaoViewItemTransacao.ts
@@ -5,12 +5,10 @@ export class AdicionColunaIdTransacaoViewItemTransacao1721848566340 implements M
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "item_transacao" ADD "idTransacaoView" character varying NULL`);
-        await queryRunner.query(`ALTER TABLE "file" ALTER COLUMN "id" SET DEFAULT uuid_generate_v4()`);
-        await queryRunner.query(`ALTER TABLE "user" ADD CONSTRAINT "FK_User_photo_ManyToOne" FOREIGN KEY ("photoId") REFERENCES "file"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "file" ALTER COLUMN "id" SET DEFAULT uuid_generate_v4()`);        
     }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" DROP CONSTRAINT "FK_User_photo_ManyToOne"`);
+    public async down(queryRunner: QueryRunner): Promise<void> {        
         await queryRunner.query(`ALTER TABLE "file" ALTER COLUMN "id" DROP DEFAULT`);
         await queryRunner.query(`ALTER TABLE "item_transacao" DROP COLUMN "idTransacaoView"`);
     }
