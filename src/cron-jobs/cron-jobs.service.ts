6a2fe987cca36da422e01a02376d71daf2c65026
Raphael Rivas <raphaelrivas@hotmail.com>
2024-10-18 10:12:05 -0300

    test: dont run cronjobs on load

--- src/cron-jobs/cron-jobs.service.ts
+++ src/cron-jobs/cron-jobs.service.ts
@@ -83,8 +83,6 @@ export class CronJobsService {
   }
 
   async onModuleLoad() {
-    await this.generateRemessaVanzeiros();
-    await this.syncTransacaoViewOrdem('generateRemessaVanzeiros');
     const THIS_CLASS_WITH_METHOD = 'CronJobsService.onModuleLoad';
 
     this.jobsConfig.push(
