d62443d127e981a5df2d2a42794454e5e985c35d
williamfl2007 <williamfl2007@gmail.com>
2024-10-29 09:54:20 -0300

    criacao de novas entidades

---
+++ src/cnab/entity/NovoRemessa/ordens-pagamentos-agrupadas.entity.ts
@@ -0,0 +1,54 @@
+import { EntityHelper } from 'src/utils/entity-helper';
+import {  Column, CreateDateColumn, DeepPartial, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity()
+export class OrdemPagamentoAgrupadoEntity extends EntityHelper {
+  constructor(dto?: DeepPartial<OrdemPagamentoAgrupadoEntity>) {
+    super();
+    if (dto) {
+      Object.assign(this, dto);      
+    }
+  }
+
+  @PrimaryGeneratedColumn({ primaryKeyConstraintName: 'PK_Ordem_Pagamento_Agrupado_id' })
+  id: number;  
+  
+  @Column({ type: String, unique: false, nullable: false })
+  userId: number;
+
+  @Column({ type: String, unique: false, nullable: false })
+  userBankCode: string;
+
+  @Column({ type: String, unique: false, nullable: false })
+  userBankAgency: string;
+
+  @Column({ type: String, unique: false, nullable: false })
+  userBankAccount: string;
+
+  @CreateDateColumn()
+  dataPagamento: Date;
+
+  @Column({
+    type: 'decimal',
+    unique: false,
+    nullable: true,
+    precision: 13,
+    scale: 5,
+  })
+  ValorTotal: number;
+
+  @Column({ type: String, unique: false, nullable: false })
+  ordensPagamentoId: string[];  
+
+  @Column({ type: String, unique: false, nullable: false })
+  statusRemessa: string; 
+  
+  @Column({type: Boolean, unique: false, nullable: true})
+  isPago: boolean;  
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;  
+}
