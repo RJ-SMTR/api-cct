5622f6219479b9c9dc3f45ad3882b7bb8637b06e
williamfl2007 <williamfl2007@gmail.com>
2024-11-14 17:12:36 -0300

    preparar vanzeiros

--- src/cron-jobs/cron-jobs.service.ts
+++ src/cron-jobs/cron-jobs.service.ts
@@ -83,6 +83,8 @@ export class CronJobsService {
   }
 
   async onModuleLoad() {     
+    await this.generateRemessaVanzeiros();
+    await this.syncTransacaoViewOrdem('van');
     const THIS_CLASS_WITH_METHOD = 'CronJobsService.onModuleLoad';
     this.jobsConfig.push(
       {
@@ -369,13 +371,13 @@ export class CronJobsService {
     this.logger.log('Tarefa iniciada', METHOD);
     const startDate = new Date();
     const today = debug?.today || new Date();
-    const sex = subDays(today, 7);
-    const qui = subDays(today, 1);
+    const sex = subDays(today, 6);
+    const qui = today;
 
     await this.cnabService.saveTransacoesJae(sex, qui, 0,'Van');
-    const listCnab = await this.cnabService.generateRemessa({ tipo: PagadorContaEnum.ContaBilhetagem
-      , dataPgto: today, isConference: false, isCancelamento: false, isTeste: false });
-    await this.cnabService.sendRemessa(listCnab);
+    // const listCnab = await this.cnabService.generateRemessa({ tipo: PagadorContaEnum.ContaBilhetagem
+    //   , dataPgto: today, isConference: false, isCancelamento: false, isTeste: false });
+    // await this.cnabService.sendRemessa(listCnab);
     this.logger.log(`Tarefa finalizada - ${formatDateInterval(new Date(), startDate)}`, METHOD);
   }
 
