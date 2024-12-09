70da434963a5054048a77de43d1860b600155a69
William Ferreira Leite <williamfl2007@gmail.com>
2024-12-09 09:32:09 -0300

    Update cron-jobs.service.ts
    Rodar report

--- src/cron-jobs/cron-jobs.service.ts
+++ src/cron-jobs/cron-jobs.service.ts
@@ -83,8 +83,7 @@ export class CronJobsService {
   }
 
   async onModuleLoad() {    
-    await this.sendStatusReport();
-    
+    await this.sendStatusReport();    
     const THIS_CLASS_WITH_METHOD = 'CronJobsService.onModuleLoad';
     this.jobsConfig.push(
       {
