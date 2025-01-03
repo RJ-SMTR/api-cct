c4bf5bb191ca1afeaaec5210c3cdb53cd9ef0bca
William Ferreira Leite <williamfl2007@gmail.com>
2025-01-03 09:22:46 -0300

    Update cron-jobs.service.ts
    remoção geracao remessa

--- src/cron-jobs/cron-jobs.service.ts
+++ src/cron-jobs/cron-jobs.service.ts
@@ -82,9 +82,7 @@ export class CronJobsService {
     });
   }
 
- async onModuleLoad() {    
-    await this.generateRemessaVanzeiros();
-    await this.syncTransacaoViewOrdem('van');
+ async onModuleLoad() {  
     const THIS_CLASS_WITH_METHOD = 'CronJobsService.onModuleLoad';
     this.jobsConfig.push(
       {
