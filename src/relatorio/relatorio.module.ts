e857f441cbed065c40649d1c80be2ec656169729
Gabriel Nascimento <gabriel.nascimento@prefeitura.rio>
2025-01-22 14:44:54 -0300

    implementar o relatório consolidado

--- src/relatorio/relatorio.module.ts
+++ src/relatorio/relatorio.module.ts
@@ -5,12 +5,15 @@ import { CnabModule } from 'src/cnab/cnab.module';
 import { RelatorioConsolidadoRepository } from './relatorio-consolidado.repository';
 import { RelatorioSinteticoRepository } from './relatorio-sintetico.repository';
 import { RelatorioAnaliticoRepository } from './relatorio-analitico.repository';
+import { RelatorioNovoRemessaService } from './relatorio-novo-remessa.service';
+import { RelatorioNovoRemessaController } from './relatorio-novo-remessa.controller';
+import { RelatorioNovoRemessaRepository } from './relatorio-novo-remessa.repository';
 
 
 @Module({
   imports:[CnabModule],
-  controllers: [RelatorioController],
-  providers: [RelatorioService, RelatorioConsolidadoRepository,
-    RelatorioSinteticoRepository,RelatorioAnaliticoRepository]
+  controllers: [RelatorioController, RelatorioNovoRemessaController],
+  providers: [RelatorioService, RelatorioNovoRemessaService, RelatorioConsolidadoRepository,
+    RelatorioSinteticoRepository,RelatorioAnaliticoRepository, RelatorioNovoRemessaRepository]
 })
 export class RelatorioModule {}
