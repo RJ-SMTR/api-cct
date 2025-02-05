ace950e611ef4ee039fae367eb09ee3fd387c2c3
William Ferreira Leite <williamfl2007@gmail.com>
2025-02-05 09:22:36 -0300

    Update cron-jobs.service.ts
    Comentado retorno para testes do pod reiniciando

--- src/cron-jobs/cron-jobs.service.ts
+++ src/cron-jobs/cron-jobs.service.ts
@@ -130,7 +130,7 @@ export class CronJobsService {
         cronJobParameters: {
           cronTime: '*/30 * * * *', //  Every 30 min
           onTick: async () => {
-            await this.retornoExec();
+            //await this.retornoExec();
           },
         },
       },
