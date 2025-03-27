0340ecc6a096324ef2991424d38f80ecf7dc6bee
williamfl2007 <williamfl2007@gmail.com>
2025-03-27 17:18:55 -0300

    remessa com valores mockados por decisao judicial desconto de 6%

--- src/cnab/novo-remessa/convertTo/header-lote-to-detalhe-a.convert.ts
+++ src/cnab/novo-remessa/convertTo/header-lote-to-detalhe-a.convert.ts
@@ -9,33 +9,59 @@ import { Cnab104FormaParcelamento } from "src/cnab/enums/104/cnab-104-forma-parc
 
 @Injectable()
 export class HeaderLoteToDetalheA {
-    
-    static logger = new CustomLogger(HeaderLoteToDetalheA.name, { timestamp: true });   
 
-    constructor() { }
+  static logger = new CustomLogger(HeaderLoteToDetalheA.name, { timestamp: true });
 
-    static async convert(headerLote: HeaderLote,ordem: OrdemPagamentoAgrupado,nsr?: number,
-       hist?:OrdemPagamentoAgrupadoHistorico,numeroDocumento?: number){     
-       
-        const da = new DetalheADTO();
-        da.headerLote = headerLote;
-        da.nsr = nsr;        
-        da.dataVencimento = hist?.dataReferencia ?? ordem.dataPagamento;
-        da.periodoVencimento = ordem.dataPagamento; 
-        da.valorLancamento = ordem.valorTotal;   
-        da.valorRealEfetivado = ordem.valorTotal;    
-        da.numeroDocumentoEmpresa = numeroDocumento;
-        da.indicadorBloqueio = 'N';
-        da.quantidadeMoeda = 0;
-        da.indicadorFormaParcelamento = Cnab104FormaParcelamento.DataFixa.toString();  
-        da.finalidadeDOC = "00";
-        da.loteServico = headerLote.formaLancamento === '41'?1:2;     
-        da.tipoMoeda ='BRL';  
-        da.numeroDocumentoBanco = "0";          
-        da.numeroParcela = 1;
-        da.quantidadeParcelas = 1;   
-        if(hist)
-          da.ordemPagamentoAgrupadoHistorico = hist;
-        return da;
+  constructor() { }
+
+  static async convert(headerLote: HeaderLote, ordem: OrdemPagamentoAgrupado, nsr?: number,
+    hist?: OrdemPagamentoAgrupadoHistorico, numeroDocumento?: number) {
+    const da = new DetalheADTO();
+
+    const nomeConsorcio = ordem.ordensPagamento[0].nomeConsorcio;
+    if (nomeConsorcio === 'Intersul') {
+      da.valorLancamento = 251407.70;
+      da.valorRealEfetivado = 251407.70;
     }
+
+    if (nomeConsorcio === 'Transcarioca') {
+      da.valorLancamento = 493902.78;
+      da.valorRealEfetivado = 493902.78;
+    }
+
+    if (nomeConsorcio === 'Internorte') {
+      da.valorLancamento = 557045.54;
+      da.valorRealEfetivado = 557045.54;
+    }
+
+    if (nomeConsorcio === 'MobiRio') {
+      da.valorLancamento = ordem.valorTotal;
+      da.valorRealEfetivado = ordem.valorTotal;
+    }
+
+    if (nomeConsorcio === 'Santa Cruz') {
+      da.valorLancamento = 476517.54;
+      da.valorRealEfetivado = 476517.54;
+    }
+
+    da.headerLote = headerLote;
+    da.nsr = nsr;
+    da.dataVencimento = hist?.dataReferencia ?? ordem.dataPagamento;
+    da.periodoVencimento = ordem.dataPagamento;
+    da.valorLancamento = ordem.valorTotal;
+    da.valorRealEfetivado = ordem.valorTotal;
+    da.numeroDocumentoEmpresa = numeroDocumento;
+    da.indicadorBloqueio = 'N';
+    da.quantidadeMoeda = 0;
+    da.indicadorFormaParcelamento = Cnab104FormaParcelamento.DataFixa.toString();
+    da.finalidadeDOC = "00";
+    da.loteServico = headerLote.formaLancamento === '41' ? 1 : 2;
+    da.tipoMoeda = 'BRL';
+    da.numeroDocumentoBanco = "0";
+    da.numeroParcela = 1;
+    da.quantidadeParcelas = 1;
+    if (hist)
+      da.ordemPagamentoAgrupadoHistorico = hist;
+    return da;
+  }
 }
