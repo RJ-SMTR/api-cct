39f986eefbc3d20b20f0d8ff87ec7d6fff6f394d
williamfl2007 <williamfl2007@gmail.com>
2025-05-16 10:45:58 -0300

    rodar modal

--- src/cron-jobs/cron-jobs.service.ts
+++ src/cron-jobs/cron-jobs.service.ts
@@ -103,7 +103,7 @@ export class CronJobsService {
   }
 
   async onModuleLoad(){   
-    await this.readRetornoExtrato();
+    await this.remessaModalExec();
     const THIS_CLASS_WITH_METHOD = 'CronJobsService.onModuleLoad';
     this.jobsConfig.push(
       {
