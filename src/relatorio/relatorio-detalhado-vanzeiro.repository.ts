abb4260c3e56f1a3fa0c5eb9faa0d5bcf5e76b0c
Gabriel0109 <82231197+Gabriel0109@users.noreply.github.com>
2025-06-05 16:13:00 -0300

    fix: ajuste valores somados

--- src/relatorio/relatorio-detalhado-vanzeiro.repository.ts
+++ src/relatorio/relatorio-detalhado-vanzeiro.repository.ts
@@ -15,30 +15,37 @@ export class RelatorioDetalhadoRepository {
   async findDetalhadoVanzeiro(args: { userId: number;dataInicio: Date; dataFim: Date; }) {
     const dataInicio = args.dataInicio.toISOString().slice(0,10)
     const dataFim = args.dataFim.toISOString().slice(0,10)
-    let query = ` select distinct da.id, da."dataVencimento",da."valorLancamento" valor,
-                  case when ap."isPago"=true then 'Pago'
-                  else 'Não Pago' end as status ,
-                  oc."message" motivo
-                  from item_transacao it         
-                    inner join item_transacao_agrupado ita on it."itemTransacaoAgrupadoId"=ita.id
-                    inner join detalhe_a da on da."itemTransacaoAgrupadoId"=ita.id
-                    inner join arquivo_publicacao ap on ap."itemTransacaoId"=it.id
-                    inner join cliente_favorecido cf on cf.id=it."clienteFavorecidoId"
-                    inner join ocorrencia oc on oc."detalheAId" = da."id"
-                    inner join public.user pu on pu."cpfCnpj"=cf."cpfCnpj"
-                    inner join header_lote hl on hl.id = da."headerLoteId"
-                    inner join header_arquivo ha on ha.id = hl."headerArquivoId"
-                  where it."nomeConsorcio" in('STPC','STPL','TEC')
+    let query = ` select data."dataVencimento", sum(data."valorLancamento") as valor, data.status, data.motivo
+from (
+        select distinct
+            da.id, da."dataVencimento", da."valorLancamento",
+case
+    when ap."isPago" = true then 'Pago'
+    else 'Não Pago'
+end as status,
+oc."message" motivo
+        from
+            item_transacao it
+            inner join item_transacao_agrupado ita on it."itemTransacaoAgrupadoId" = ita.id
+            inner join detalhe_a da on da."itemTransacaoAgrupadoId" = ita.id
+            inner join arquivo_publicacao ap on ap."itemTransacaoId" = it.id
+            inner join cliente_favorecido cf on cf.id = it."clienteFavorecidoId"
+            inner join ocorrencia oc on oc."detalheAId" = da."id"
+            inner join public.user pu on pu."cpfCnpj" = cf."cpfCnpj"
+            inner join header_lote hl on hl.id = da."headerLoteId"
+            inner join header_arquivo ha on ha.id = hl."headerArquivoId"
+        where
+                  it."nomeConsorcio" in('STPC','STPL','TEC')
                   and da."dataVencimento" between '${dataInicio}' and '${dataFim}'	
                      AND ita."idOrdemPagamento"  NOT LIKE '%U%'
                    and ha.status <> '5'
                   and pu."id"=${args.userId}
-                  group by    da.id,
-    cf."nome",
-    da."dataVencimento",
-    oc."message",
-    status,
-    ap."isPago"
+      ) as data
+group by
+    data."dataVencimento",
+    data.status,
+    data.motivo
+order by data."dataVencimento"
                   `;  
     const queryRunner = this.dataSource.createQueryRunner();
     await queryRunner.connect();
