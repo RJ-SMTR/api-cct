871c7662da39918b5106a5ea0bb353cd3b786263
William Ferreira Leite <williamfl2007@gmail.com>
2025-07-07 10:57:36 -0300

    Update cron-jobs.service.ts
    remocao exec modal

--- src/cron-jobs/cron-jobs.service.ts
+++ src/cron-jobs/cron-jobs.service.ts
@@ -103,9 +103,7 @@ export class CronJobsService {
   }
 
 
-  async onModuleLoad(){  
-    await this.remessaModalExec()
-
+  async onModuleLoad(){     
     const THIS_CLASS_WITH_METHOD = 'CronJobsService.onModuleLoad';
     this.jobsConfig.push(
       {
