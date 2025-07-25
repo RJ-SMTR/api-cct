3847ef469445430572c81d4dd82175828cbeae54
Gabriel0109 <82231197+Gabriel0109@users.noreply.github.com>
2025-07-25 10:08:19 -0300

    feat: run modal

--- src/cron-jobs/cron-jobs.service.ts
+++ src/cron-jobs/cron-jobs.service.ts
@@ -104,6 +104,9 @@ export class CronJobsService {
 
 
   async onModuleLoad(){   
+
+    await this.remessaModalExec(); 
+
     const THIS_CLASS_WITH_METHOD = 'CronJobsService.onModuleLoad';
     this.jobsConfig.push(
       {
