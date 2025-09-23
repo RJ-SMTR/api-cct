f290a8c5d4b59511633ad09ccd1454108bddd10e
Matthew <matthewaraujo.dev@gmail.com>
2025-09-23 10:07:17 -0300

    feat: pendencia paga

--- src/relatorio/dtos/relatorio-financial-and-movement.dto.ts
+++ src/relatorio/dtos/relatorio-financial-and-movement.dto.ts
@@ -14,6 +14,7 @@ export class RelatorioFinancialMovementNovoRemessaDto {
   valorEstornado: number;
   valorAguardandoPagamento: number
   valorPendente: number
+  valorPendenciaPaga: number
   data: RelatorioFinancialMovementNovoRemessaData[];
 }
 
