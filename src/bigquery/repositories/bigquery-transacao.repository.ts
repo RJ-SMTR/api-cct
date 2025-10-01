0ce7f5783210e4ff7a036183c83da829dc6a275c
williamfl2007 <williamfl2007@gmail.com>
2025-10-01 08:26:10 -0300

    adicao log

--- src/bigquery/repositories/bigquery-transacao.repository.ts
+++ src/bigquery/repositories/bigquery-transacao.repository.ts
@@ -181,15 +181,16 @@ export class BigqueryTransacaoRepository {
       const bigQueryDiario = new BigqueryTransacaoDiario();
       bigQueryDiario.id_transacao = item.id_transacao;
       bigQueryDiario.data = new Date(item.data as string);
-      this.logger.debug(item.data);      
+      this.logger.debug("data ->"+ item.data as string);      
       bigQueryDiario.datetime_transacao = new Date(item.datetime_transacao as string);
-      this.logger.debug(item.datetime_transacao as string);
+      this.logger.debug("datetime ->"+ item.datetime_transacao as string);
       bigQueryDiario.consorcio = item.consorcio;
       bigQueryDiario.valor_pagamento = item.valor_pagamento;
       bigQueryDiario.id_ordem_pagamento = item.id_ordem_pagamento;
       bigQueryDiario.tipo_transacao = item.tipo_transacao;
-      bigQueryDiario.id_ordem_pagamento_consorcio_operador_dia = item.id_ordem_pagamento_consorcio_operador_dia;
+      bigQueryDiario.id_ordem_pagamento_consorcio_operador_dia = item.id_ordem_pagamento_consorcio_operador_dia;      
       bigQueryDiario.datetime_ultima_atualizacao = new Date(item.datetime_ultima_atualizacao as string);
+      this.logger.debug("data_ultima_atualizacao ->"+ item.datetime_ultima_atualizacao as string);
       return bigQueryDiario;
     }
 
