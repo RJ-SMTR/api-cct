9338efcf92db74be44799f109cb116dcfe0f278a
Gabriel0109 <82231197+Gabriel0109@users.noreply.github.com>
2025-11-17 10:28:08 -0300

    Update 1762891665531-AgendamentoPagamento.ts

--- src/database/migrations/1762891665531-AgendamentoPagamento.ts
+++ src/database/migrations/1762891665531-AgendamentoPagamento.ts
@@ -5,7 +5,30 @@ export class AgendamentoPagamento1762891665531 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {   
         await queryRunner.query(`CREATE TABLE "aprovacao_pagamento" ("id" integer NOT NULL, "valorGerado" numeric(13,5) NOT NULL, "valorAprovado" numeric(13,5) NOT NULL, "dataAprovacao" date NOT NULL, "status" character varying, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "detalheAId" integer, "aprovadorId" integer, CONSTRAINT "PK_AprovacaoPagamentoId" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`CREATE TABLE "agendamento_pagamento" ("id" integer NOT NULL, "tipoBeneficiario" character varying(200), "tipoPagamento" character varying(200), "dataPagamentoUnico" date NOT NULL, "valorPagamentoUnico" numeric(13,5) NOT NULL, "motivoPagamentoUnico" character varying, "diaSemana" character varying, "horario" TIME NOT NULL, "aprovacao" boolean, "status" boolean, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "beneficiarioUsuarioId" integer, "pagadorId" integer, "responsavelId" integer, "aprovacaoPagamentoId" integer, "cadastradorId" integer, "modificadorId" integer, CONSTRAINT "PK_AgendamentoPagamentoId" PRIMARY KEY ("id"))`);
+      await queryRunner.query(`
+  CREATE TABLE "agendamento_pagamento" (
+    "id" SERIAL NOT NULL,
+    "tipoBeneficiario" character varying(200),
+    "tipoPagamento" character varying(200),
+    "dataPagamentoUnico" date,
+    "valorPagamentoUnico" numeric(13,5),
+    "motivoPagamentoUnico" character varying,
+    "diaSemana" character varying,
+    "horario" TIME,
+    "aprovacao" boolean,
+    "status" boolean,
+    "createdAt" TIMESTAMP NOT NULL DEFAULT now(),
+    "updatedAt" TIMESTAMP NOT NULL DEFAULT now(),
+    "beneficiarioUsuarioId" integer,
+    "pagadorId" integer,
+    "responsavelId" integer,
+    "aprovacaoPagamentoId" integer,
+    "cadastradorId" integer,
+    "modificadorId" integer,
+    CONSTRAINT "PK_AgendamentoPagamentoId" PRIMARY KEY ("id")
+  )
+`);
+
         await queryRunner.query(`ALTER TABLE "aprovacao_pagamento" ADD CONSTRAINT "FK_DetalheA_ManyToOne" FOREIGN KEY ("detalheAId") REFERENCES "detalhe_a"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "aprovacao_pagamento" ADD CONSTRAINT "FK_AprovadorUsuario_ManyToOne" FOREIGN KEY ("aprovadorId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "agendamento_pagamento" ADD CONSTRAINT "FK_BeneficiarioUsuario_ManyToOne" FOREIGN KEY ("beneficiarioUsuarioId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
