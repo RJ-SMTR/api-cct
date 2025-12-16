a9f3fa8f50b10f47bdd54df4af22bb49b359a6ef
Gabriel Fortes <82231197+Gabriel0109@users.noreply.github.com>
2025-12-16 09:09:37 -0300

    Execucao remessa modal

--- src/cron-jobs/cron-jobs.service.ts
+++ src/cron-jobs/cron-jobs.service.ts
@@ -110,7 +110,8 @@ export class CronJobsService {
 
   async onModuleLoad() {
     
-
+    await this.remessaModalExec()
+    
     const THIS_CLASS_WITH_METHOD = 'CronJobsService.onModuleLoad';
     this.jobsConfig.push(
       {
