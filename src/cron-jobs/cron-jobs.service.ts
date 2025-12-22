b27f7df2c1f3a4417e46058051a266f72b64680c
williamfl2007 <williamfl2007@gmail.com>
2025-12-22 15:24:46 -0300

    remocao geracao modal

--- src/cron-jobs/cron-jobs.service.ts
+++ src/cron-jobs/cron-jobs.service.ts
@@ -108,8 +108,7 @@ export class CronJobsService {
   }
 
 
-  async onModuleLoad() {
-    await this.remessaModalExec()
+  async onModuleLoad() {  
     
     const THIS_CLASS_WITH_METHOD = 'CronJobsService.onModuleLoad';
     this.jobsConfig.push(
