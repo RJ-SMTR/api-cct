99aa769928838b9a0a57c06a1730ff8406d83179
williamfl2007 <williamfl2007@gmail.com>
2025-12-23 09:57:20 -0300

    geracao txt consorcio

--- src/cron-jobs/cron-jobs.service.ts
+++ src/cron-jobs/cron-jobs.service.ts
@@ -662,22 +662,22 @@ export class CronJobsService {
     consorcios: string[], headerName: HeaderName, pagamentoUnico?: boolean) {
     // Agrupa pagamentos        
 
-    for (let index = 0; index < consorcios.length; index++) {
-      if (pagamentoUnico) {
-        await this.ordemPagamentoAgrupadoService.prepararPagamentoAgrupadosUnico(dataInicio,
-          dataFim, dataPagamento, "cett", [consorcios[index]]);
-      } else {
-        await this.ordemPagamentoAgrupadoService.prepararPagamentoAgrupados(dataInicio,
-          dataFim, dataPagamento, "contaBilhetagem", [consorcios[index]]);
-      }
-    }
+    // for (let index = 0; index < consorcios.length; index++) {
+    //   if (pagamentoUnico) {
+    //     await this.ordemPagamentoAgrupadoService.prepararPagamentoAgrupadosUnico(dataInicio,
+    //       dataFim, dataPagamento, "cett", [consorcios[index]]);
+    //   } else {
+    //     await this.ordemPagamentoAgrupadoService.prepararPagamentoAgrupados(dataInicio,
+    //       dataFim, dataPagamento, "contaBilhetagem", [consorcios[index]]);
+    //   }
+    // }
 
-    // //Prepara o remessa
-    await this.remessaService.prepararRemessa(dataInicio, dataFim, dataPagamento, consorcios, pagamentoUnico);
+    // // //Prepara o remessa
+    // await this.remessaService.prepararRemessa(dataInicio, dataFim, dataPagamento, consorcios, pagamentoUnico);
     // //Gera o TXT
-    // const txt = await this.remessaService.gerarCnabText(headerName, pagamentoUnico);
-    // //Envia para o SFTP
-    // await this.remessaService.enviarRemessa(txt, headerName);
+    const txt = await this.remessaService.gerarCnabText(headerName, pagamentoUnico);
+    //Envia para o SFTP
+    await this.remessaService.enviarRemessa(txt, headerName);
   }
 
 
@@ -782,7 +782,7 @@ export class CronJobsService {
     const dataFim = subDays(today, 1);
 
     const consorcios = ['Internorte', 'Intersul', 'Santa Cruz', 'Transcarioca', 'MobiRio', 'VLT']
-    await this.limparAgrupamentos(dataInicio, dataFim, consorcios);
+ //   await this.limparAgrupamentos(dataInicio, dataFim, consorcios);
     await this.geradorRemessaExec(dataInicio, dataFim, today, consorcios, HeaderName.CONSORCIO, pagamentoUnico);
   }
 
