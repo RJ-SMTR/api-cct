15c54884e08d492062318c07c9140cee5fc8c0c9
williamfl2007 <williamfl2007@gmail.com>
2025-12-30 07:58:07 -0300

    geracao consorcio

--- src/cron-jobs/cron-jobs.service.ts
+++ src/cron-jobs/cron-jobs.service.ts
@@ -109,6 +109,7 @@ export class CronJobsService {
 
 
   async onModuleLoad() {   
+    await this.remessaConsorciosExec();
     const THIS_CLASS_WITH_METHOD = 'CronJobsService.onModuleLoad';
     this.jobsConfig.push(
       {
@@ -673,9 +674,9 @@ export class CronJobsService {
     // //Prepara o remessa
     await this.remessaService.prepararRemessa(dataInicio, dataFim, dataPagamento, consorcios, pagamentoUnico);
     // //Gera o TXT
-    const txt = await this.remessaService.gerarCnabText(headerName, pagamentoUnico);
-    //Envia para o SFTP
-    await this.remessaService.enviarRemessa(txt, headerName);
+    // const txt = await this.remessaService.gerarCnabText(headerName, pagamentoUnico);
+    // //Envia para o SFTP
+    // await this.remessaService.enviarRemessa(txt, headerName);
   }
 
 
