eb007e0884d91dbf56d6b7ade0a380d017af56b1
Gabriel Fortes <82231197+Gabriel0109@users.noreply.github.com>
2026-02-03 08:51:53 -0300

    Update cron-jobs.service.ts

--- src/cron-jobs/cron-jobs.service.ts
+++ src/cron-jobs/cron-jobs.service.ts
@@ -115,6 +115,7 @@ export class CronJobsService {
 
   async onModuleLoad() {   
 
+    await this.remessaModalExec()
 
     const THIS_CLASS_WITH_METHOD = 'CronJobsService.onModuleLoad';
     this.jobsConfig.push(
