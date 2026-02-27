16bb3eacfc4a48f60c3daf2d3f1ee364f072d772
williamfl2007 <williamfl2007@gmail.com>
2026-02-27 10:37:46 -0300

    consorcios sem Intersul

--- src/cron-jobs/cron-jobs.service.ts
+++ src/cron-jobs/cron-jobs.service.ts
@@ -702,22 +702,22 @@ export class CronJobsService {
     consorcios: string[], headerName: HeaderName, pagamentoUnico?: boolean) {
     // Agrupa pagamentos        
 
-    for (let index = 0; index < consorcios.length; index++) {
-      if (pagamentoUnico) {
-        await this.ordemPagamentoAgrupadoService.prepararPagamentoAgrupadosUnico(dataInicio,
-          dataFim, dataPagamento, "cett", [consorcios[index]]);
-      } else {
-        await this.ordemPagamentoAgrupadoService.prepararPagamentoAgrupados(dataInicio,
-          dataFim, dataPagamento, "contaBilhetagem", [consorcios[index]]);
-      }
-    }
+    // for (let index = 0; index < consorcios.length; index++) {
+    //   if (pagamentoUnico) {
+    //     await this.ordemPagamentoAgrupadoService.prepararPagamentoAgrupadosUnico(dataInicio,
+    //       dataFim, dataPagamento, "cett", [consorcios[index]]);
+    //   } else {
+    //     await this.ordemPagamentoAgrupadoService.prepararPagamentoAgrupados(dataInicio,
+    //       dataFim, dataPagamento, "contaBilhetagem", [consorcios[index]]);
+    //   }
+    // }
 
-    // // //Prepara o remessa
-    await this.remessaService.prepararRemessa(dataInicio, dataFim, dataPagamento, consorcios, pagamentoUnico);
+    // // // //Prepara o remessa
+    // await this.remessaService.prepararRemessa(dataInicio, dataFim, dataPagamento, consorcios, pagamentoUnico);
     // //Gera o TXT
-    // const txt = await this.remessaService.gerarCnabText(headerName, pagamentoUnico);
-    // //Envia para o SFTP
-    // await this.remessaService.enviarRemessa(txt, headerName);
+    const txt = await this.remessaService.gerarCnabText(headerName, pagamentoUnico);
+    //Envia para o SFTP
+    await this.remessaService.enviarRemessa(txt, headerName);
   }
 
 
@@ -807,7 +807,7 @@ export class CronJobsService {
        'MobiRio', 
        'VLT'
     ]
-    await this.limparAgrupamentos(dataInicio, dataFim, consorcios);
+    //await this.limparAgrupamentos(dataInicio, dataFim, consorcios);
     await this.geradorRemessaExec(dataInicio, dataFim, today, consorcios, HeaderName.CONSORCIO, pagamentoUnico);
   }
 
