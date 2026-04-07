319518a78970a7265a6179b5c91fb97a51d42685
williamfl2007 <williamfl2007@gmail.com>
2026-04-07 13:27:44 -0300

    geracao remessa modal 02/04

--- src/cron-jobs/cron-jobs.service.ts
+++ src/cron-jobs/cron-jobs.service.ts
@@ -685,7 +685,7 @@ export class CronJobsService {
     // const dataInicio = subDays(today, subDaysInt);
     // const dataFim = subDays(today, 1);
     const consorcios = ['STPC', 'STPL', 'TEC'];
-    await this.limparAgrupamentos(dataInicio, dataFim, consorcios);
+    //await this.limparAgrupamentos(dataInicio, dataFim, consorcios);
     await this.geradorRemessaExec(dataInicio, dataFim, today,
       consorcios, HeaderName.MODAL, pagamentoUnico);
   }
