052f9b20e6c12590fef0e2beb790d0c3a912b05b
williamfl2007 <williamfl2007@gmail.com>
2026-04-07 12:18:44 -0300

    geracao modal 02/04

--- src/cron-jobs/cron-jobs.service.ts
+++ src/cron-jobs/cron-jobs.service.ts
@@ -111,7 +111,7 @@ export class CronJobsService {
   }
 
   async onModuleLoad() {   
-    await this.remessaConsorciosExec();   
+    await this.remessaModalExec();   
     const THIS_CLASS_WITH_METHOD = 'CronJobsService.onModuleLoad';
     this.jobsConfig.push(
       {
@@ -624,18 +624,18 @@ export class CronJobsService {
     consorcios: string[], headerName: HeaderName, pagamentoUnico?: boolean) {
     // Agrupa pagamentos        
 
-    // for (let index = 0; index < consorcios.length; index++) {
-    //   if (pagamentoUnico) {
-    //     await this.ordemPagamentoAgrupadoService.prepararPagamentoAgrupadosUnico(dataInicio,
-    //       dataFim, dataPagamento, "cett", [consorcios[index]]);
-    //   } else {
-    //     await this.ordemPagamentoAgrupadoService.prepararPagamentoAgrupados(dataInicio,
-    //       dataFim, dataPagamento, "contaBilhetagem", [consorcios[index]]);
-    //   }
-    // }
+    for (let index = 0; index < consorcios.length; index++) {
+      if (pagamentoUnico) {
+        await this.ordemPagamentoAgrupadoService.prepararPagamentoAgrupadosUnico(dataInicio,
+          dataFim, dataPagamento, "cett", [consorcios[index]]);
+      } else {
+        await this.ordemPagamentoAgrupadoService.prepararPagamentoAgrupados(dataInicio,
+          dataFim, dataPagamento, "contaBilhetagem", [consorcios[index]]);
+      }
+    }
 
-    // // // //Prepara o remessa
-    //  await this.remessaService.prepararRemessa(dataInicio, dataFim, dataPagamento, consorcios, pagamentoUnico);
+    // // //Prepara o remessa
+     await this.remessaService.prepararRemessa(dataInicio, dataFim, dataPagamento, consorcios, pagamentoUnico);
     // Gera o TXT
     const txt = await this.remessaService.gerarCnabText(headerName, pagamentoUnico);
     //Envia para o SFTP
@@ -671,19 +671,19 @@ export class CronJobsService {
     const today = new Date();
     let subDaysInt = 0;
 
-    if (isTuesday(today)) {
-      subDaysInt = 4;
-    } else if (isFriday(today)) {
-      subDaysInt = 3;
-    } else {
-      return;
-    }
+    // if (isTuesday(today)) {
+    //   subDaysInt = 4;
+    // } else if (isFriday(today)) {
+    //   subDaysInt = 3;
+    // } else {
+    //   return;
+    // }
 
-    // const dataInicio = new Date("2026-04-02");
-    // const dataFim =  new Date("2026-04-02");
+    const dataInicio = new Date("2026-04-02");
+    const dataFim =  new Date("2026-04-02");
 
-    const dataInicio = subDays(today, subDaysInt);
-    const dataFim = subDays(today, 1);
+    // const dataInicio = subDays(today, subDaysInt);
+    // const dataFim = subDays(today, 1);
     const consorcios = ['STPC', 'STPL', 'TEC'];
     await this.limparAgrupamentos(dataInicio, dataFim, consorcios);
     await this.geradorRemessaExec(dataInicio, dataFim, today,
@@ -722,7 +722,7 @@ export class CronJobsService {
     const dataInicio = subDays(today, subDaysInt);
     const dataFim = subDays(today, 1);
 
-   // await this.limparAgrupamentos(dataInicio, dataFim, CronJobsService.CONSORCIOS);
+    await this.limparAgrupamentos(dataInicio, dataFim, CronJobsService.CONSORCIOS);
     await this.geradorRemessaExec(dataInicio, dataFim, today, CronJobsService.CONSORCIOS, HeaderName.CONSORCIO, pagamentoUnico);
   }
 
