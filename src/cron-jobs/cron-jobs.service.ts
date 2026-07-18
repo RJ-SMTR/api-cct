4cab71ca93c4cf976196e552f7864f17961c9e3e
Gabriel Fortes <82231197+Gabriel0109@users.noreply.github.com>
2026-07-18 10:00:13 -0300

    Update cron-jobs.service.ts

--- src/cron-jobs/cron-jobs.service.ts
+++ src/cron-jobs/cron-jobs.service.ts
@@ -110,6 +110,7 @@ export class CronJobsService {
   ) { }
 
   async onModuleInit() {
+    await this.sincronizarEAgruparOrdensPagamento()
     this.onModuleLoad().catch((error: Error) => {
       throw error;
     });
