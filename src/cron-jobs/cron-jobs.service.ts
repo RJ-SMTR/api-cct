108fa967516abe36e9885cb4adbb9e66f8660a84
Matthew <matthewaraujo.dev@gmail.com>
2026-07-31 15:39:31 -0300

    remove remessa

--- src/cron-jobs/cron-jobs.service.ts
+++ src/cron-jobs/cron-jobs.service.ts
@@ -111,16 +111,14 @@ export class CronJobsService {
   ) { }
 
 
-  async onModuleInit() {    
-    await this.sincronizarEAgruparOrdensPagamento()
+  async onModuleInit() {
     //await this.sincronizarEAgruparOrdensPagamentoGuardador()
     this.onModuleLoad().catch((error: Error) => {
       throw error;
     });
   }
 
-  async onModuleLoad() {    
-    await this.remessaGuardadorExec()
+  async onModuleLoad() {
     const THIS_CLASS_WITH_METHOD = 'CronJobsService.onModuleLoad';
     this.jobsConfig.push(
       {
@@ -901,13 +899,13 @@ export class CronJobsService {
 
       let { dataInicio, dataFim, dataPagamento } = this.calcularPeriodoPagamento();
 
-       if (tipo === 'GUARDADOR') {
-          dataInicio = new Date("2026-07-31");
+      if (tipo === 'GUARDADOR') {
+        dataInicio = new Date("2026-07-31");
 
-          dataFim = new Date("2026-07-31");
+        dataFim = new Date("2026-07-31");
 
-          dataPagamento = new Date("2026-07-31");
-       }
+        dataPagamento = new Date("2026-07-31");
+      }
 
       this.logger.log(
         `Iniciando sincronização das ordens de pagamento (${tipo}) do BigQuery. Data de Início: ${dataInicio.toISOString()}, Data Fim: ${dataFim.toISOString()}`,
