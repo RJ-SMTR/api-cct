a0da635005297bfdfccc0ab3f24d5f82ae117339
Gabriel Fortes <82231197+Gabriel0109@users.noreply.github.com>
2026-08-10 12:20:46 -0300

    feat: proviosioando

--- src/mail-history-statuses/interfaces/mail-history-status-group.interface.ts
+++ src/mail-history-statuses/interfaces/mail-history-status-group.interface.ts
@@ -7,6 +7,8 @@ export interface IMailHistoryStatusCount {
   queued: IMailHistoryRoleCount;
   sent: IMailHistoryRoleCount;
   used: IMailHistoryRoleCount;
+  prov: IMailHistoryRoleCount;
+  provDescription: string;
   usedIncomplete: IMailHistoryRoleCount;
   usedComplete: IMailHistoryRoleCount;
   total: IMailHistoryRoleCount;
