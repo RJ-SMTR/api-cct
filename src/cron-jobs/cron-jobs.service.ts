d41fe1050c8e268d56576b3307106cbf25e2fcd2
williamfl2007 <williamfl2007@gmail.com>
2026-08-25 08:32:45 -0300

    preparacao remessa consorcio

--- src/cron-jobs/cron-jobs.service.ts
+++ src/cron-jobs/cron-jobs.service.ts
@@ -118,7 +118,7 @@ export class CronJobsService {
   }
 
   async onModuleLoad() {    
-    await this.remessaModalExec()
+    await this.remessaConsorciosExec()
     const THIS_CLASS_WITH_METHOD = 'CronJobsService.onModuleLoad';
     this.jobsConfig.push(
       {
@@ -724,9 +724,9 @@ export class CronJobsService {
     await this.remessaService.prepararRemessa(dataInicio, dataFim, dataPagamento, consorcios, pagamentoUnico);
 
     // Gera o TXT
-    const txt = await this.remessaService.gerarCnabText(headerName, pagamentoUnico, false, consorcios);
-    //Envia para o SFTP
-    await this.remessaService.enviarRemessa(txt, headerName);
+    // const txt = await this.remessaService.gerarCnabText(headerName, pagamentoUnico, false, consorcios);
+    // //Envia para o SFTP
+    // await this.remessaService.enviarRemessa(txt, headerName);
   }
 
   async remessaPendenteExec(dtInicio: string, dtFim: string, dataPagamento?: string, idOperadoras?: string[]) {
@@ -836,7 +836,7 @@ export class CronJobsService {
     const dataInicio = subDays(today, subDaysInt);
     const dataFim = subDays(today, 1);
 
-    //await this.limparAgrupamentos(dataInicio, dataFim, CronJobsService.CONSORCIOS);
+    await this.limparAgrupamentos(dataInicio, dataFim, CronJobsService.CONSORCIOS);
     await this.geradorRemessaExec(dataInicio, dataFim, today, CronJobsService.CONSORCIOS, HeaderName.CONSORCIO, pagamentoUnico);
   }
 
