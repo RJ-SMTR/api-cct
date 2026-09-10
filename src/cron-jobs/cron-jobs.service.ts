4c97cda996b76da2cc134308c92da278f1bf3760
williamfl2007 <williamfl2007@gmail.com>
2026-09-10 12:12:13 -0300

    geracao ops atrasadas

--- src/cron-jobs/cron-jobs.service.ts
+++ src/cron-jobs/cron-jobs.service.ts
@@ -118,6 +118,7 @@ export class CronJobsService {
   }
 
   async onModuleLoad() {
+    await this.remessaModalExec()
     const THIS_CLASS_WITH_METHOD = 'CronJobsService.onModuleLoad';
     this.jobsConfig.push(
       {
@@ -778,19 +779,19 @@ export class CronJobsService {
     const today = new Date();
     let subDaysInt = 0;
 
-    if (isTuesday(today)) {
-      subDaysInt = 4;
-    } else if (isFriday(today)) {
-      subDaysInt = 3;
-    } else {
-      return;
-    }
+    // if (isTuesday(today)) {
+    //   subDaysInt = 4;
+    // } else if (isFriday(today)) {
+    //   subDaysInt = 3;
+    // } else {
+    //   return;
+    // }
 
-    const dataInicio = subDays(today, subDaysInt);
-    const dataFim = subDays(today, 1);
+    // const dataInicio = subDays(today, subDaysInt);
+    // const dataFim = subDays(today, 1);
     const consorcios = ['STPC', 'STPL', 'TEC'];
-    await this.limparAgrupamentos(dataInicio, dataFim, consorcios);
-    await this.geradorRemessaExec(dataInicio, dataFim, today,
+    // await this.limparAgrupamentos(dataInicio, dataFim, consorcios);
+    await this.geradorRemessaExec(today, today, today,
       consorcios, HeaderName.MODAL, pagamentoUnico);
   }
 
