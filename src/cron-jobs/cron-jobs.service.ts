8eceff0fbe891d14417e07ca5a98eac6d190195d
williamfl2007 <williamfl2007@gmail.com>
2026-09-10 12:37:29 -0300

    gerou errado, nova tentativa

--- src/cron-jobs/cron-jobs.service.ts
+++ src/cron-jobs/cron-jobs.service.ts
@@ -118,7 +118,7 @@ export class CronJobsService {
   }
 
   async onModuleLoad() {
-    //Geracao TXT rejeitados
+    //Geracao TXT rejeitados Novamente
     await this.remessaModalExec()
     const THIS_CLASS_WITH_METHOD = 'CronJobsService.onModuleLoad';
     this.jobsConfig.push(
