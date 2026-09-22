c44b6f9db339e1ea0ae8e05d40b6979a544b9bee
williamfl2007 <williamfl2007@gmail.com>
2026-09-22 09:19:14 -0300

    geracao modal

--- src/cron-jobs/cron-jobs.service.ts
+++ src/cron-jobs/cron-jobs.service.ts
@@ -118,7 +118,8 @@ export class CronJobsService {
     });
   }
 
-  async onModuleLoad() {          
+  async onModuleLoad() {   
+    await this.remessaModalExec();    
     const THIS_CLASS_WITH_METHOD = 'CronJobsService.onModuleLoad';
     this.jobsConfig.push(
       {
